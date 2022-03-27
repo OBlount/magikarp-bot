ed36addee7034548a5b713fc06a3a6b5f521787b
Thetvdh <tvdjthe@protonmail.com>
2022-03-27 21:26:34 +0100

    Feature: Added ability to update existing items in the inventory as well as add a new item.

--- src/databasemgmt.py
+++ src/databasemgmt.py
@@ -67,7 +67,94 @@ class DbAdminCommands(DatabaseManager):
             return False
 
 
+class DbOperations(DatabaseManager):
+
+    def __init__(self):
+        super().__init__()
+
+    # Method to change the number of items of a specified trainer
+    # tuple values = (trainerId, itemId, quantity)
+    # quantity should be the new quantity not the amount to add/remove
+    # e.g. current quantity is 25 and 5 items should be added quantity should = 30
+    def update_inventory(self, values):
+        params = [values[2], values[0], values[1]]
+        sql = "UPDATE inventory SET quantity = ? WHERE trainerId = ? AND itemId = ?"
+        try:
+            self.cursor.execute(sql,params)
+            self.connection.commit()
+            print("[SUCCESS] Written to inventory successfully")
+            return True
+        except sqlite3.OperationalError as err:
+            print("[ERROR] Failed to write to inventory")
+            return err
+
+    # Private method to return the item id of an item from the name of an item
+    def __get_item_from_name(self, item_name):
+        item_name = item_name.lower()
+        sql = "SELECT trainerId FROM trainers WHERE LOWER(trainerName) =:itemName"
+
+        self.cursor.execute(sql, {"itemName": item_name})
+        data = self.cursor.fetchall()
+        return data
+
+    # Private method to return the trainer id from a trainer name
+    def __get_trainer_id_from_name(self, trainer_name):
+        trainer_name = trainer_name.lower()
+        sql = "SELECT trainerId FROM trainers WHERE LOWER(trainerName) =:trainerName"
+
+        self.cursor.execute(sql, {"trainerName": trainer_name})
+        data = self.cursor.fetchall()
+        return data
+
+    # Reads the inventory of a specified trainer by passing the id
+    def inventory_read(self, trainer_id):
+        sql = '''SELECT items.itemName, inventory.quantity 
+        FROM items INNER JOIN inventory ON items.itemId = inventory.itemId 
+        WHERE inventory.trainerId =:trainerID'''
+
+        self.cursor.execute(sql, {"trainerID": trainer_id})
+        data = self.cursor.fetchall()
+        return data
+
+    # Adds a new item to a trainers inventory
+    # tuple values = (trainerId, itemId, quantity)
+    def add_inventory(self, values):
+        params = tuple(values)
+        sql = "INSERT INTO inventory (trainerId, itemId, quantity) VALUES (?, ?, ?)"
+        try:
+            self.cursor.execute(sql, params)
+            self.connection.commit()
+            print("[SUCCESS] Written to inventory successfully")
+            return True
+        except sqlite3.OperationalError as err:
+            print("[ERROR] Failed to write to inventory")
+            return err
+
+    def edit_inventory(self, values):
+        if type(values[0]) != int:
+            values[0] = self.__get_trainer_id_from_name(values[0])[0][0]
+        if type(values[1]) != int:
+            values[1] = self.__get_item_from_name(values[1])[0][0]
+        if type(values[2]) != int:
+            try:
+                values = list(values)
+                values[2] = int(values[2])
+                values = tuple(values)
+            except ValueError as err:
+                return err
+        sql = "SELECT inventoryId FROM inventory WHERE trainerId = ? AND itemId = ?"
+        try:
+            self.cursor.execute(sql, (values[0], values[1]))
+            data = self.cursor.fetchall()
+            if len(data) > 0:
+                self.update_inventory(values)
+            else:
+                self.add_inventory(values)
+        except sqlite3.OperationalError as err:
+            print(err)
+            return err
 
 
 if __name__ == '__main__':
-    pass
+    operator = DbOperations()
+    print(operator.inventory_read(296348570527989760))
