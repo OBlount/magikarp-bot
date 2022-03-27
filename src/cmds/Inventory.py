8bf3e8d20ae470aeee07d1e7674d0dd8bf31aebe
Thetvdh <tvdjthe@protonmail.com>
2022-03-27 21:38:36 +0100

    Feature: Implemented the bot sending the inventory to the user.

--- src/cmds/Inventory.py
+++ src/cmds/Inventory.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-
+from src.databasemgmt import DbOperations
 
 class Inventory(commands.Cog):
 
@@ -7,10 +7,17 @@ class Inventory(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.operator = DbOperations()
 
     @commands.command(name=CMD)
     async def list(self, ctx):
-        await ctx.send(f"<@{ctx.author.id}>'s inventory: ...")
+        data = self.operator.inventory_read(296348570527989760)
+        message = f"<@{ctx.author.id}>'s inventory:\n"
+
+        for item in data:
+            message = message + str(item[0]) + ": " + str(item[1]) + "\n"
+
+        await ctx.send(message)
 
 
 if __name__ == "__main__":
