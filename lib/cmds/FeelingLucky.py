e492ec97fbad78d182804f98532943adb8372535
Owen Blount <oblount077@gmail.com>
2022-03-28 21:23:09 +0100

    Feature: Added item giving to FeelingLucky command

--- lib/cmds/FeelingLucky.py
+++ lib/cmds/FeelingLucky.py
@@ -16,14 +16,26 @@ class Feeling_Lucky(commands.Cog):
         rand = random.randint(1, 3)
 
         match rand:
+            # Item Event - Add an item to the player's inventory
             case 1:
-                message = rand
+                item_name, quantity = self.__give_item(ctx.author.id)
+                message = f"<@{ctx.author.id}> got {quantity} {item_name}(s)!\n"
             case 2:
                 message = rand
             case 3:
                 message = rand
 
         await ctx.send(message)
+
+    def __give_item(self, trainer_id):
+        number_of_items = OPERATOR.get_max_item()
+        random_id = random.randint(1, number_of_items)
+        random_quantity = random.randint(1, 3)
+
+        val = (trainer_id, random_id, random_quantity)
+        OPERATOR.edit_inventory(val)
+
+        return (OPERATOR.get_item_name_from_id(random_id), random_quantity)
 
 
 if __name__ == "__main__":
